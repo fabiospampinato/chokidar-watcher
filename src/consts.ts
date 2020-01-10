ce2a7d99c08b555099e3824eb3ee552ba49f676d
Fabio Spampinato <spampinabio@gmail.com>
2020-01-10 19:56:41 +0000

    More reliable “rename” detection (doubled timeout)

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 
 /* CONSTS */
 
-const RENAME_TIMEOUT = 500; // Amount of time to wait for the complementary add/unlink event //TODO: Having a timeout for these sorts of things isn't very reliable
+const RENAME_TIMEOUT = 1000; // Amount of time to wait for the complementary add/unlink event //TODO: Having a timeout for these sorts of things isn't very reliable
 
 /* EXPORT */
 
